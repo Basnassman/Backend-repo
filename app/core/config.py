c4660719f2de206cf5088495ff676e1f362cae86
Basnassman <167700926+Basnassman@users.noreply.github.com>
2026-04-23 12:33:28 +0000

    important

--- app/core/config.py
+++ app/core/config.py
@@ -31,7 +31,7 @@ class Config:
     # -------- LLM --------
     LLAMA_API_URL: str = _get_env(
         "LLAMA_API_URL",
-        "http://54.227.171.175:3000/chat"   # 🔥 endpoint واضح
+        "http://54.227.171.175:3000/completion"   # 🔥 endpoint واضح
     )
 
     API_KEY: str = _get_env(
